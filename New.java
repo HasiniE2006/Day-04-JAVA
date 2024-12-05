8db340dc7b1e89d01446b54708e56fb8e00bc42f
HasiniE2006 <hasini1530@gmail.com>
2024-12-05 16:24:21 +0530

    Add files via upload

---
+++ New.java
@@ -0,0 +1,20 @@
+public class New {
+        public static void main(String args[])
+        {
+            int x=1;
+            int n=10;
+            while(x<=n)
+            {
+             if((x%2)==0)
+             {
+                System.out.println(x);
+             }
+             if((x%2)!=0)
+             {
+                System.out.println(x);
+             }
+             x++;
+            }
+        }
+    }
+    

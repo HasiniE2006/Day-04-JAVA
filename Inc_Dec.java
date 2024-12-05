0bbe12c5019f7b6cb9f2a644f21b4a44915a7aac
HasiniE2006 <hasini1530@gmail.com>
2024-12-05 15:59:34 +0530

    Add files via upload

---
+++ Inc_Dec.java
@@ -0,0 +1,14 @@
+public class Inc_Dec {
+    public static void main(String args[])
+    {
+        int n=5;
+        System.out.println(n++);
+        int a=4;
+        System.out.println(++a);
+        int b=6;
+        System.out.println(--b);
+        int c=56;
+        System.out.println(c++);
+    }
+    
+}
